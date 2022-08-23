9177b1663bb5a95b33cfd91f71847312df17c90c
belalhossainS <mdbelal01792@gmail.com>
2022-08-23 15:10:08 +0600

    Alert

--- js/player.js
+++ js/player.js
@@ -1,3 +1,57 @@
-function addToCart(player){
-    console.log(player)
+const cartArray = [];
+
+function display(cartProduct) {
+    console.log(cartProduct);
+
+    const tableBody = document.getElementById('cart-products');
+    if (cartProduct.length >= 6) {
+        alert('you are not eqal')
+        return;
+    }
+    tableBody.innerHTML = '';
+    for (let i = 0; i < cartProduct.length; i++) {
+        // console.log(cartArray[i].productName,);
+        const name = cartArray[i].productName;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        `;
+
+        tableBody.appendChild(tr);
+    }
 }
+
+function addToCart(element) {
+    // console.log(element.parentNode.parentNode.children[0].innerText)
+
+    const productName = element.parentNode.parentNode.children[0].innerText;
+    // console.log(productName);
+
+    const productObj = {
+        productName: productName,
+    }
+    cartArray.push(productObj)
+    console.log(cartArray.length);
+
+    display(cartArray);
+}
+
+
+
+// Bugest Calculate part 
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+
+    const calculateField = document.getElementById('player-field');
+    const calculateAmountString = calculateField.value;
+    const calculateTotal = parseFloat(calculateAmountString);
+
+
+   const calculateButton = document.getElementById('calculate-btn');
+   const calculateButtonString = calculateButton.innerText;
+   calculateButton.innerText =calculateTotal;
+
+   console.log(calculateTotal);
+})
